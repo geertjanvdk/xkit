a991f62ea146f70995c4d6e417f36b797d568ebc
Geert Vanderkelen <geert@vanderkelen.org>
2021-02-20 18:09:49 +0100

    Add extra informational methods to xsql.DB
    We add teh following methods:
    * HaveTable
    * HaveIndex
    * and ColumnInfo returning useful information about a column for
      a table

--- xsql/db.go
+++ xsql/db.go
@@ -62,6 +62,41 @@ func Open(driverName, dataSourceName string) (*DB, error) {
 	}, nil
 }
 
+// HaveTable checks whether table is available.
+func (db *DB) HaveTable(table string) (bool, error) {
+	q := `SELECT COUNT(*) as cnt FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = database() AND TABLE_NAME = ?`
+
+	var cnt int
+
+	err := db.QueryRow(q, table).Scan(&cnt)
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, fmt.Errorf("failed checking existance of table `%s` (%s)", table, err)
+	}
+
+	return cnt > 0, nil
+}
+
+// HaveConstraint checks whether constraint is available for table.
+func (db *DB) HaveIndex(table string, index string) (bool, error) {
+	q := `SELECT COUNT(*) as cnt FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = database() AND INDEX_NAME = ?`
+
+	var cnt int
+
+	err := db.QueryRow(q, table, index).Scan(&cnt)
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return false, fmt.Errorf("failed checking existance of table index `%s(%s)` (%s)",
+			table, index, err)
+	}
+
+	return cnt > 0, nil
+}
+
 // HaveConstraint checks whether constraint is available for table.
 func (db *DB) HaveConstraint(table string, constraint string) (bool, error) {
 	q := `SELECT COUNT(*) as cnt FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
@@ -74,7 +109,8 @@ WHERE CONSTRAINT_SCHEMA = database() AND TABLE_NAME = ? AND CONSTRAINT_NAME = ?`
 	case err == sql.ErrNoRows:
 		return false, nil
 	case err != nil:
-		return false, fmt.Errorf("failed checking table constraint `%s(%s)` (%s)", table, constraint, err)
+		return false, fmt.Errorf("failed checking existance of table constraint `%s(%s)` (%s)",
+			table, constraint, err)
 	}
 
 	return cnt > 0, nil
@@ -92,8 +128,47 @@ WHERE EVENT_OBJECT_SCHEMA = database() AND EVENT_OBJECT_TABLE = ? AND TRIGGER_NA
 	case err == sql.ErrNoRows:
 		return false, nil
 	case err != nil:
-		return false, fmt.Errorf("failed checking table trigger `%s(%s)` (%s)", table, trigger, err)
+		return false, fmt.Errorf("failed checking existance of table trigger `%s(%s)` (%s)",
+			table, trigger, err)
 	}
 
 	return cnt > 0, nil
+}
+
+type ColumnInfo struct {
+	TableName        string
+	Name             string
+	DataType         string
+	Nullable         bool
+	CharMaxLength    *int
+	CharOctetLength  *int
+	PartOfPrimaryKey bool
+}
+
+func (db *DB) ColumnInfo(table, column string) (*ColumnInfo, error) {
+	q := `SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE, IF(IS_NULLABLE='YES', TRUE, FALSE),
+       CHARACTER_MAXIMUM_LENGTH, CHARACTER_OCTET_LENGTH,
+       IF(COLUMN_KEY='PRI', TRUE, FALSE) AS part_of_pk
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = database() AND TABLE_NAME = ? AND COLUMN_NAME = ?`
+
+	col := &ColumnInfo{}
+
+	err := db.QueryRow(q, table, column).Scan(
+		&col.TableName,
+		&col.Name,
+		&col.DataType,
+		&col.Nullable,
+		&col.CharMaxLength,
+		&col.CharOctetLength,
+		&col.PartOfPrimaryKey,
+	)
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, nil
+	case err != nil:
+		return nil, fmt.Errorf("failed getting column `%s.%s` (%s)", table, column, err)
+	}
+
+	return col, nil
 }

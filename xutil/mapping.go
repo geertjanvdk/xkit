1778cc8cfde1bb73ee544584e413aa70444107d7
Geert Vanderkelen <geert@vanderkelen.org>
2021-03-02 20:05:14 +0100

    Add OrderedMap as map keeping insertion order
    We add OrderedMap which  wraps around a Go map keeping the order
    with which elements have been added. Keys must be strings, but values
    can be anything (interface{}).
    Unlike map, index assigment is not possible. Use the `Set` method to
    set a key with a particular value.
    Use the Keys method to retrieves keys, Values to get the values. To
    get both, which probably what you want, use the KeysValues method.

---
+++ xutil/mapping.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2021, Geert JM Vanderkelen
+
+package xutil
+
+import "sync"
+
+// OrderedMap wraps around a Go map keeping the order with which
+// elements have been added. Keys must be strings, but values
+// can be anything (interface{}).
+// Unlike map, index assigment is not possible. Use the `Set`
+// method to set a key with a particular value.
+// Use the Keys method to retrieves keys, Values to get the
+// values. To get both, which probably what you want, use
+// the KeysValues method.
+type OrderedMap struct {
+	mapMU sync.RWMutex
+	map_  map[string]interface{}
+	order []string
+}
+
+// Set key in OrderedMap to value. Previously stored values
+// are overwritten, but the order does not change.
+func (om *OrderedMap) Set(key string, value interface{}) {
+	om.mapMU.Lock()
+	defer om.mapMU.Unlock()
+
+	if om.map_ == nil {
+		om.map_ = map[string]interface{}{}
+	}
+
+	om.map_[key] = value
+	if !HasString(om.order, key) {
+		om.order = append(om.order, key)
+	}
+}
+
+// Keys returns keys as slice of string.
+func (om *OrderedMap) Keys() []string {
+	om.mapMU.RLock()
+	defer om.mapMU.RUnlock()
+
+	return om.order
+}
+
+func (om *OrderedMap) values() []interface{} {
+	res := make([]interface{}, len(om.order))
+	for i, k := range om.order {
+		res[i] = om.map_[k]
+	}
+	return res
+}
+
+// Values returns the values as slice of interfaces.
+func (om *OrderedMap) Values() []interface{} {
+	om.mapMU.RLock()
+	defer om.mapMU.RUnlock()
+
+	return om.values()
+}
+
+// KeysValues returns the keys as slice of strings, and values as slice of interfaces.
+func (om *OrderedMap) KeysValues() ([]string, []interface{}) {
+	om.mapMU.RLock()
+	defer om.mapMU.RUnlock()
+
+	return om.order, om.values()
+}
